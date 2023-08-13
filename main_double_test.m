cc687964c1e78cf7061faa8ab7b6543ea84feb55
melih-pelit <mmpelit@gmail.com>
2023-08-13 21:11:43 +0900

    do the polygon plotting\

--- main_double_test.m
+++ main_double_test.m
@@ -120,11 +120,61 @@ for j = 1:length(results)
 end
 
 %% Polygon visualization TODO
-figure()
-pgon = polyshape([-24 -24 12 12 0],[-440 260 0 -540 400])
-plot(pgon)
-%%
-save_disturbance_test_result(dist_test)
+
+results_to_be_plotted = {dist_test_nom.dist_test.results, dist_test_vslipsl.dist_test.results};
+print = true;
+save_names = {"nominal", "vslipsl"};
+
+for i = 1:length(results_to_be_plotted)
+    % detect the edge points
+    results = results_to_be_plotted(i);
+    results = results{1};
+    pass_idx = find(results(:,1)==1);
+    pass_results = results(pass_idx,:);
+    
+    mins_x = [];
+    maxs_x = [];
+    
+    mins_y = [];
+    maxs_y = [];
+    
+    for j = 1:length(pass_results)
+        % 
+        curr_F_x = pass_results(j,2);
+    
+        if ismember(curr_F_x, maxs_x)
+            continue
+        end
+    
+        curr_idx = find(pass_results(:,2)==curr_F_x);
+        curr_F_ys = pass_results(curr_idx,3);
+    
+        mins_x(end+1) = curr_F_x;
+        maxs_x(end+1) = curr_F_x;
+        
+        mins_y(end+1) = min(curr_F_ys);
+        maxs_y(end+1) = max(curr_F_ys);
+    end
+    
+    polygon_x = [mins_x, fliplr(maxs_x)];
+    polygon_y = [mins_y, fliplr(maxs_y)];
+    
+    figure()
+    pgon = polyshape(polygon_x,polygon_y);
+    plot(pgon)
+    xlim([-100, 200])
+    ylim([-1000, 800])
+    xlabel("$F_x$ [N]", 'Interpreter','latex')
+    ylabel("$F_y$ [N]", 'Interpreter','latex')
+
+    if print
+        addpath("C:\Matlab Workspace\altmany-export_fig-d8b9f4a")
+        set(gcf, 'color', 'none');
+        set(gca, 'color', 'none');
+        export_fig("figures/dist_res_"+ save_names{i} + ".png", '-m3')
+    end
+
+end
 
 %%
 function save_disturbance_test_result(dist_test)
